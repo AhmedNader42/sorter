737e3a41c9216d404b26f6407894b8cfbaa35861
ahmednader424 <67971175+ahmednader424@users.noreply.github.com>
2021-03-25 15:37:17 +0200

    "Sorter class initialized and sort function implemented for numbers"

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+class Sorter {
+    constructor(public collection: number[] | string) {}
+
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                // Type guard
+                if (this.collection instanceof Array) {
+                    // this.collection.
+                    // For sorting numbers
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+
+                // For sorting string
+                if (typeof this.collection === "string") {
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([1, 10, -1, 2, 3]);
+sorter.sort();
+console.log(sorter.collection);
